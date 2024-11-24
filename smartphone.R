4622b42bc2fd973d9fac130551c702d6698b3156
Ziyan Humji <zh24aaf@herts.ac.uk>
2024-11-24 15:15:43 +0000

    Filtering Dataset

--- smartphone.R
+++ smartphone.R
@@ -3,3 +3,11 @@ library(readr)
 smartphone<-read_csv("smartphone_cleaned_v5.csv")
 #Renaming the Dataset
 df<-smartphone
+
+#Filters the data frame to include only apple and samsung brands
+
+filtered_df <- smartphone[smartphone$brand_name %in% c('apple', 'samsung'), ]
+#Printing the filtered dataset
+print(filtered_df)
+#Printing column names
+colnames(filtered_df)
